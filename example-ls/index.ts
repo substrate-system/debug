70a23550042cb06fcd1e14c4ba5fd74ed4b8b255
nichoth <nichoth@nichoth.com>
2025-08-18 22:51:14 -0700

    better example

--- example-ls/index.ts
+++ example-ls/index.ts
@@ -1,4 +1,19 @@
 import Debug from '../src/browser/index.js'
-const debug = Debug('abc:*')
+const debug = Debug('abc:def')
+
+// this corresponds to setting localStorage in index.html
+const debug2 = Debug('abc:123')
+
+const debug3 = Debug('abc:456')
 
 debug('hello world')
+
+debug2('hello two...')
+
+debug3('456...')
+
+localStorage.setItem('DEBUG', 'abc:*')
+
+debug3('456 after setting localStorage...')
+
+localStorage.setItem('DEBUG', '')
