26eb84d8f2179ce2d525086d1eb652aba11657d2
nichoth <nichoth@nichoth.com>
2025-10-19 13:28:01 -0700

    fix tests for new API

--- test/index.ts
+++ test/index.ts
@@ -83,19 +83,7 @@ test('env object parameter - non-matching namespace', async t => {
         'Should not log when env object DEBUG does not match')
 })
 
-test('Without namespace in development mode', async t => {
-    const code = `
-        import createDebug from '${nodeJsPath}'
-        const debug = createDebug()
-        debug('dev mode logging')
-    `
-
-    const { stderr } = await runChildProcess(code, { NODE_ENV: 'development' })
-    t.ok(stderr.includes('dev mode logging'),
-        'Should log in development mode without namespace')
-})
-
-test('Without namespace in production mode', async t => {
+test('Without namespace', async t => {
     const code = `
         import createDebug from '${nodeJsPath}'
         const debug = createDebug()
@@ -104,19 +92,7 @@ test('Without namespace in production mode', async t => {
 
     const { stderr } = await runChildProcess(code, { NODE_ENV: 'production' })
     t.ok(!stderr.includes('should not appear'),
-        'Should not log in production mode without namespace')
-})
-
-test('With env object NODE_ENV=test', async t => {
-    const code = `
-        import createDebug from '${nodeJsPath}'
-        const debug = createDebug(null, { NODE_ENV: 'test' })
-        debug('test env mode')
-    `
-
-    const { stderr } = await runChildProcess(code)
-    t.ok(stderr.includes('test env mode'),
-        'Should log when NODE_ENV=test in env object')
+        'Should not log without namespace')
 })
 
 test('Format with multiple arguments', async t => {
