d095c39ba1ae7687e42ab3c5e3704566a91e719f
nichoth <nichoth@nichoth.com>
2025-12-24 17:13:20 -0800

    fix default export

--- src/noop.ts
+++ src/noop.ts
@@ -2,4 +2,4 @@ import { type Debugger } from './index.js'
 
 export const noop:Debugger = function (_args:any[]) {}
 noop.extend = function (_namespace:string) { return noop }
-
+export default noop
